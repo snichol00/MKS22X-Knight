1fe89eb504327fd8da1de62e07cd99efd93c4047
Sophie Nichol <snichol00@stuy.edu>
2019-02-12 10:14:06 -0500

    started KnightBoard

---
+++ KnightBoard.java
@@ -0,0 +1,28 @@
+public class KnighBoard{
+  //@throws IllegalArgumentException when either parameter is negative.
+  //Initialize the board to the correct size and make them all 0's
+  public KnightBoard(int startingRows,int startingCols){
+
+  }
+
+  public String toString(){
+
+  }
+
+  //@throws IllegalStateException when the board contains non-zero values.
+  //@throws IllegalArgumentException when either parameter is negative or out of bounds.
+  public boolean solve(int startingRow, int startingCol){
+
+  }
+
+  //@throws IllegalStateException when the board contains non-zero values.
+  //@throws IllegalArgumentException when either parameter is negative or out of bounds.
+  public int countSolutions(int startingRow, int startingCol){
+
+  }
+
+  // level is the # of the knight
+  private boolean solveH(int row ,int col, int level){
+
+  }
+}
